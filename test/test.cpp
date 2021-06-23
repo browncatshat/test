97a15884fda8e2b553cf170d789770ffdc966fd5
browncatshat <86170709+browncatshat@users.noreply.github.com>
2021-06-23 15:34:20 -0400

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,9 @@
 #define CATCH_CONFIG_MAIN
+#include <catch2/internal/catch_default_main.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <lib.hpp>
+
+
 
 TEST_CASE("add") {
     REQUIRE(add(1, 2) == 3);
